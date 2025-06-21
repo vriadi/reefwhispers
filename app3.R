92e524321bf3181f7d5e6aea7efe6faef2ae1a0a
vriadi <vanessariadi@gmail.com>
2025-06-21 17:01:57 +0800

    app 3 troubleshooting

--- app3.R
+++ app3.R
@@ -11,8 +11,8 @@ library(visNetwork)
 library(plotly)
 
 # Load your data
-messages <- read_csv("../data/communications_full.csv")
-df <- read_excel("../data/Nadia Conti_copy.xlsx") %>%
+messages <- read_csv("data/communications_full.csv")
+df <- read_excel("data/Nadia Conti_copy.xlsx") %>%
   clean_names()
 
 # UI
